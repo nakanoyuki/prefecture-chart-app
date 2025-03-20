ade5d0dbba2c8d87d137bce096c6c418239e22ab
nakanoyuki <yukich41@gmail.com>
2025-03-20 15:56:12 +0900

    都道府県のデータを取得して表示

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import { PrefecturesCheckbox } from "./components/PrefecturesCheckbox";
+import { fetchPrefectures } from "./lib/fetchPrefectures";
+
+function App() {
+  const [loading, setLoading] = useState(false);
+  const [prefectures, setPrefectures] = useState<
+    { prefCode: number; prefName: string }[]
+  >([]);
+  const [error, setError] = useState<string | null>(null);
+
+  const getPrefectures = async () => {
+    setLoading(true);
+    setError(null);
+    try {
+      const data = await fetchPrefectures();
+
+      setPrefectures(data);
+      console.log(data);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getPrefectures();
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p style={{ color: "red" }}>{error}</p>;
+
+  return (
+    <>
+      <div>
+        {prefectures.map((prefecture) => (
+          <PrefecturesCheckbox
+            key={prefecture.prefCode}
+            prefName={prefecture.prefName}
+          />
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/lib/fetchPrefectures.tsx
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const API_URL = import.meta.env.VITE_API_URL as string;
+const API_KEY = import.meta.env.VITE_API_KEY as string;
+
+export type Prefecture = {
+  prefCode: number;
+  prefName: string;
+};
+
+export const fetchPrefectures = async (): Promise<Prefecture[]> => {
+  try {
+    const response = await axios.get(API_URL, {
+      headers: {
+        "X-API-KEY": API_KEY,
+        "Content-Type": "application/json; charset=UTF-8",
+      },
+    });
+
+    return response.data.result;
+  } catch (error) {
+    console.error("API取得エラー:", error);
+    throw new Error("都道府県データの取得に失敗しました");
+  }
+};
